5956aeed43ec6fcb6fb81f98d62ef6f3a956adad
Nguyen Phong <gp20092004@gmail.com>
2026-04-18 09:57:47 +0700

    cap nhat gui email2

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -23,6 +23,7 @@ const nodemailer = require('nodemailer');
 const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
     service: 'Gmail',
+    secure: true, 
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,

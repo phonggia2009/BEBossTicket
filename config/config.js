85263a077d67c641b1533b602f80cf6a42a05351
Nguyen Phong <gp20092004@gmail.com>
2026-04-21 16:29:16 +0700

    Cập nhật múi giờ

--- config/config.js
+++ config/config.js
@@ -17,13 +17,23 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'postgres'
+    dialect: 'postgres',
+     timezone: '+07:00', // Đặt múi giờ Việt Nam (+7)
+    dialectOptions: {
+      dateStrings: true,
+      typeCast: true,
+    },
   },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    timezone: '+07:00', // Đặt múi giờ Việt Nam (+7)
+    dialectOptions: {
+      dateStrings: true,
+      typeCast: true,
+    },
   }
 };

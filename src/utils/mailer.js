cd48ec1f26a482d97e905b6b615cf68cf77d5320
Nguyen Phong <gp20092004@gmail.com>
2026-05-12 20:58:27 +0700

    Cập nhật mail2

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -21,7 +21,7 @@
 // const dns = require('dns');
 // dns.setDefaultResultOrder('ipv4first');
 
-// const nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer');
  
  
 exports.sendEmail = async (to, subject, html, attachments = []) => {

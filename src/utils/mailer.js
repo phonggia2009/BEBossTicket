870bf95f687706aa084065024a845797ae458bb5
Nguyen Phong <gp20092004@gmail.com>
2026-05-12 20:52:10 +0700

    Cập nhật mail

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -24,46 +24,26 @@
 // const nodemailer = require('nodemailer');
  
  
-// exports.sendEmail = async (to, subject, html, attachments = []) => {
-//   const transporter = nodemailer.createTransport({
-//     host: 'smtp.gmail.com', 
-//     port: 587,
-//     secure: false,  
-//     requireTLS: true,       
-//     family: 4,              
-//     auth: {
-//       user: process.env.SMTP_USER,
-//       pass: process.env.SMTP_PASS,
-//     },
-//   });
- 
-//   const mailOptions = {
-//     from: '"BossTicket Support" <no-reply@bossticket.com>',
-//     to: to,
-//     subject: subject,
-//     html: html,
-//     attachments
-//   };
- 
-//   return await transporter.sendMail(mailOptions);
-// };
-const { Resend } = require('resend');
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 exports.sendEmail = async (to, subject, html, attachments = []) => {
-  const formattedAttachments = attachments.map(att => ({
-    filename: att.filename,
-    content: att.content,      // Buffer được hỗ trợ trực tiếp
-  }));
-
-  const { data, error } = await resend.emails.send({
-    from: 'BossTicket <onboarding@resend.dev>',
-    to,
-    subject,
-    html,
-    attachments: formattedAttachments,
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com', 
+    port: 587,
+    secure: false,  
+    requireTLS: true,       
+    family: 4,              
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
   });
-
-  if (error) throw new Error(error.message);
-  return data;
+ 
+  const mailOptions = {
+    from: '"BossTicket Support" <no-reply@bossticket.com>',
+    to: to,
+    subject: subject,
+    html: html,
+    attachments
+  };
+ 
+  return await transporter.sendMail(mailOptions);
 };

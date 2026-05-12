7c39144ae64ebcc1f8b9757172e5786547428416
Nguyen Phong <gp20092004@gmail.com>
2026-05-12 21:45:07 +0700

    fix smtp 465

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -4,23 +4,21 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  requireTLS: true,
-
-  tls: {
-    family: 4,
-    rejectUnauthorized: false,
-  },
+  port: 465,
+  secure: true,
 
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
 
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
+  tls: {
+    rejectUnauthorized: false,
+  },
+
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
 });
 
 transporter.verify((error, success) => {
@@ -39,7 +37,7 @@ const sendEmail = async (
   attachments = []
 ) => {
   return await transporter.sendMail({
-    from: `"BossTicket Support" <${process.env.SMTP_USER}>`,
+    from: process.env.SMTP_USER,
     to,
     subject,
     html,

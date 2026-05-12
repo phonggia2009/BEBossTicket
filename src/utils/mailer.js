a436b5f781d9136feb0b4bd7b5769c2ed958a239
Nguyen Phong <gp20092004@gmail.com>
2026-05-12 21:31:26 +0700

    Cập nhật mail5

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -1,51 +1,32 @@
-// const nodemailer = require('nodemailer');
-
-// const transporter = nodemailer.createTransport({
-//   host: process.env.MAIL_HOST,
-//   port: process.env.MAIL_PORT,
-//   auth: {
-//     user: process.env.MAIL_USER,
-//     pass: process.env.MAIL_PASS,
-//   },
-// });
-
-// exports.sendEmail = async (to, subject, html) => {
-//   const mailOptions = {
-//     from: `Cinema Việt <${process.env.MAIL_FROM}>`,
-//     to,
-//     subject,
-//     html,
-//   };
-//   return await transporter.sendMail(mailOptions);
-// };
-// const dns = require('dns');
-// dns.setDefaultResultOrder('ipv4first');
 require("dotenv").config();
-const nodemailer = require('nodemailer');
- 
- 
-exports.sendEmail = async (to, subject, html, attachments = []) => {
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com', 
-    port: 587,
-    secure: false,  
-    requireTLS: true,       
-    family: 4,              
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
-    },
+
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first");
+
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
+
+module.exports.sendEmail = async (
+  to,
+  subject,
+  html,
+  attachments = []
+) => {
+  return transporter.sendMail({
+    from: process.env.SMTP_USER,
+    to,
+    subject,
+    html,
+    attachments,
   });
- 
-  const mailOptions = {
-    from: '"BossTicket Support" <no-reply@bossticket.com>',
-    to: to,
-    subject: subject,
-    html: html,
-    attachments
-  };
- 
-  return await transporter.sendMail(mailOptions);
 };
-
-
